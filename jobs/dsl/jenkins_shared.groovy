5bc6b9735a4c4498c609869d003b918f9db2eabf
psandomierski <psandomierski@sii.pl>
2020-12-04 15:28:44 +0100

    Play with multibranch job

--- jobs/dsl/jenkins_shared.groovy
+++ jobs/dsl/jenkins_shared.groovy
@@ -1,18 +1,9 @@
 multibranchPipelineJob('jenkins-shared') {
     displayName('jenkins-shared')
     branchSources {
-        git {
+        github {
             id('jenkins-shared')
-            remote('https://github.com/patryksandomierski/jenkins-casc.git')
-        }
-    }
-    configure {
-        def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
-        traits << 'com.cloudbees.jenkins.plugins.bitbucket.BranchDiscoveryTrait' {
-            strategyId(1) // detect all branches -refer the plugin source code for various options
-        }
-        traits << 'com.cloudbees.jenkins.plugins.bitbucket.OriginPullRequestDiscoveryTrait' {
-            strategyId(1)
+            repository('https://github.com/patryksandomierski/jenkins-casc.git')
         }
     }
     factory {
